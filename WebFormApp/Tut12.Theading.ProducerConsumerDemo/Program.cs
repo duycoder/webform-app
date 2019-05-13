45081b5bae051a559aad1bdeb5d11b2d7abe3b27
duycoder <duyprogrammer1996@gmail.com>
2019-05-13 13:01:31 +0700

    from home with thread

---
+++ WebFormApp/Tut12.Theading.ProducerConsumerDemo/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut12.Theading.ProducerConsumerDemo
+{
+    class Program
+    {
+        Queue<int> numbers = new Queue<int>();
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
